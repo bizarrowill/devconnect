8eb2356c0717dc73040673b11c77c23e57046329
William Robertson <williamp.robertson@gmail.com>
2018-05-15 20:32:09 -0700

    mongo db connected via mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://admin:admin@ds031278.mlab.com:31278/devconnector"
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Conect to MongoDB via Mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hola el World"));
 
